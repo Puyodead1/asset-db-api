c66df6377bc9f01399a66955b931cbacc4e4d75d
Puyodead1 <23562356riley@gmail.com>
2021-10-12 19:10:26 -0400

    remove config

---
+++ src/config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 5000,
+  jwtSecret: "secret",
+  jwtIssuer: "localhost",
+  jwtAudience: "localhost",
+  mongoURI: "",
+};
--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  port: 5000,
-  jwtSecret: "secret",
-  jwtIssuer: "localhost",
-  jwtAudience: "localhost",
-  mongoURI:
-    "mongodb+srv://Puyodead1:JuRi2003!@cluster0.mrn2s.mongodb.net/assetdb?retryWrites=true&w=majority",
-};
